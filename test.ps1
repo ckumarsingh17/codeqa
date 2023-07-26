ab0fc963db8ce80edd0ddd414994706769d81340
ckumarsingh17 <121482562+ckumarsingh17@users.noreply.github.com>
2023-07-26 19:44:52 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,21 @@
+# Download FSLogix installation files
+$fslogixInstallerUrl = "<fslogix_installer_url>"
+$fslogixInstallerPath = "C:\Temp\FSLogixInstaller.exe" # Change to desired installation path
+Invoke-WebRequest -Uri $fslogixInstallerUrl -OutFile $fslogixInstallerPath
+
+# Install FSLogix
+Start-Process -FilePath $fslogixInstallerPath -ArgumentList "/quiet" -Wait
+
+# Enable FSLogix for all users on the session host
+$fslogixRegPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
+$fslogixRegValue = "Enabled"
+Set-ItemProperty -Path $fslogixRegPath -Name $fslogixRegValue -Value 1
+
+# Add FSLogix to the RDS redirection settings
+$rdpRegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$rdpRegValue = "FSLogixRedirection" 
+$rdpRegData = "1"
+Set-ItemProperty -Path $rdpRegPath -Name $rdpRegValue -Value $rdpRegData
+
+# Restart the session host to apply changes
+Restart-Computer -Force
